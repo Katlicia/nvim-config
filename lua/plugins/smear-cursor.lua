ec796279c30da6951baae637b722e85cc435d51f
Katlicia <yusuful80@gmail.com>
2026-04-17 10:21:27 +0300

    cursor animation plugin

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        stiffness = 0.8,		-- Cursor speed (0.1 - 1)
+        trailing_stiffness = 0.5,      -- Trail speed
+        distance_stop_animating = 0.8, -- Animation
+        hide_target_hack = true,
+    },
+}
