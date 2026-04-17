a0499a82bc130175b8aca88149355baa74eb060b
Katlicia <yusuful80@gmail.com>
2026-04-17 10:20:57 +0300

    beacon for cursor plugin

---
+++ lua/plugins/beacon.lua
@@ -0,0 +1,8 @@
+return {
+    "danilamihailov/beacon.nvim",
+    config = function()
+        vim.g.beacon_size = 10
+        vim.g.beacon_fade = true
+        vim.g.beacon_timeout = 500
+    end
+}
